9556952f43518df961cf22f3da0c2c71395a27f6
Kaue <kauemachado@frwk.com.br>
2021-11-17 10:42:07 -0300

    Adição de um icone para o site

--- Código.js
+++ Código.js
@@ -1,22 +1,27 @@
 var data = new Date()
-var hota = data.getHours
-var minutos = data.getMinutes
-var msg = document.getElementById('msg')
+var hota = data.getHours()
+var minutos = data.getMinutes()
+let msg = document.getElementById('msg')
 
 function carregar(){
-    var imagem= document.getElementById('fundo')
+    console.log(hora)
+    var imagem= document.getElementById('Fundo')
     msg = `${hora}+ ":" + ${minutos} `
     if(Hora>=6 && hora<12){
-        imagem.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRI_aBzsDt5jVaZrHdHrddU0zc9SqzDOMhh_Z233g8Xk1rWIPh0CUIBMHwPME1c1S4dtGM&usqp=CAU'
+        imagem.src='ImagemDaManhã.png'
         
     } else if(hora>=12 && hora<=18){
-        imagem.src='https://hanashir.files.wordpress.com/2013/06/923177_10152796901035554_1951289825_n.png'
+        imagem.src='ImagemDaTarde.png'
 
     } else{
-        imagem.src='https://images3.alphacoders.com/905/thumb-350-905410.jpg'
+        imagem.src='ImagemDaNoite.jpg'
     }
 }
 
 function abrirInfo(){
     window.open('https://github.com/MBeuaK/OnePieceWeather')
+}
+
+function DescobrirLocalizacao(){
+    alert("Funcionou")
 }
