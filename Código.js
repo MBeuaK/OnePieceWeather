90ee642db2b2c4e5509e66585fd3156350bbf1c8
Kaue <kauemachado@frwk.com.br>
2021-11-18 09:43:44 -0300

    Mudança na apresentação das informações do autor e do repositório git

--- Código.js
+++ Código.js
@@ -1,25 +1,34 @@
 var data = new Date()
 var hora = data.getHours()
 var minutos = data.getMinutes()
-let msg = document.getElementById('msg')
-
+let msg = document.getElementById('relogio')
 function carregar(){
-    console.log(msg)
-    msg.value = `${hora}:${minutos} `
+    msg.innerHTML = `${hora}:${minutos} `
     if(hora>=6 && hora<12){
-        document.body.style.background ="url('ImagemDaManhã.png')"
-        
+        document.body.style.background ="url('ImagemDaManhã2.png')"
+        document.body.style.backgroundRepeat ="no-repeat" 
+        document.body.style.backgroundSize="initial"
+
+
     } else if(hora>=12 && hora<=18){
         document.body.style.background ="url('ImagemDaTarde.png')"
+        document.body.style.backgroundRepeat ="no-repeat" 
+        document.body.style.objectFit="contain"
 
     } else if(hora>18 && hora<6){
-        document.body.style.background ="url('ImagemDaNoite.jpg')"
+        document.body.style.background ="url('ImagemDaNoite.png')"
+        document.body.style.backgroundSize="cover"
     } else{
         document.body.style.background ="url('ImagemDeFundo.png')"
+        document.body.style.backgroundSize="cover"
     }
 }
 
-function abrirInfo(){
+function infoAutor(){
+    open('https://www.linkedin.com/in/mbeuak/')
+}
+
+function repositorioGit(){
     window.open('https://github.com/MBeuaK/OnePieceWeather')
 }
 
