2f706dd96cb1db78ed2e10b31d1f18d94c0e44a4
Kaue <kauemachado@frwk.com.br>
2021-11-22 14:02:31 -0300

    Organizando o projeto

--- Código.js
+++ Código.js
@@ -5,32 +5,33 @@ let msg = document.getElementById('relogio')
 function carregar(){
     msg.innerHTML = `${hora}:${minutos} `
     if(hora>=6 && hora<12){
-        backgroundProperties('ImagemDaManhã2.png')
+        backgroundProperties('Imagens/ImagemDaManhã.png')
     } else if(hora>=12 && hora<=18){
-        backgroundProperties('ImagemDaTarde.png')
-
+        backgroundProperties('Imagens/ImagemDaTarde.png')
     } else if(hora>18 && hora<6){
-        backgroundProperties('ImagemDaNoite.png')
+        backgroundProperties('Imagens/ImagemDaNoite.png')
     } else{
-        backgroundProperties('ImagemDeFundo.png')
+        backgroundProperties('Imagens/ImagemDeFundo.png')
     }
 }
-
 function backgroundProperties(url){
     document.body.style.background =`url('${url}')`
     document.body.style.backgroundRepeat ="no-repeat" 
     document.body.style.backgroundSize="cover"
-
 }
-
 function infoAutor(){
     open('https://www.linkedin.com/in/mbeuak/')
 }
-
 function repositorioGit(){
     window.open('https://github.com/MBeuaK/OnePieceWeather')
 }
-
 function DescobrirLocalizacao(){
     alert("Funcionou")
 }
+
+fetch("https://api.hgbrasil.com/weather")
+.then(function (response){
+    return response.json();
+})
+
+console.log(response.json)
