b52ec04b78c8fe4aee794e2ec003fde645f3db6c
Kaue <kauemachado@frwk.com.br>
2021-11-23 11:20:08 -0300

    Implementação da API e adição de uma imagem padrão

--- Código.js
+++ Código.js
@@ -1,21 +1,21 @@
-var data = new Date()
-var hora = data.getHours()
-var minutos = data.getMinutes()
 let msg = document.getElementById('relogio')
+let hora;
+let temp;
 function carregar(){
-    msg.innerHTML = `${hora}:${minutos} `
-    if(hora>=6 && hora<12){
+    backgroundProperties('Imagens/ImagemDeFundo.png')
+    let strings = hora.split(":");
+    let horaCerta = strings[0];
+    if(horaCerta >= 6 && horaCerta < 12){
         backgroundProperties('Imagens/ImagemDaManhã.png')
     } else if(hora>=12 && hora<=18){
         backgroundProperties('Imagens/ImagemDaTarde.png')
     } else if(hora>18 && hora<6){
         backgroundProperties('Imagens/ImagemDaNoite.png')
-    } else{
-        backgroundProperties('Imagens/ImagemDeFundo.png')
-    }
+    } 
+    
 }
-function backgroundProperties(url){
-    document.body.style.background =`url('${url}')`
+function backgroundProperties(image){
+    document.body.style.background =`url('${image}')`
     document.body.style.backgroundRepeat ="no-repeat" 
     document.body.style.backgroundSize="cover"
 }
@@ -23,15 +23,29 @@ function infoAutor(){
     open('https://www.linkedin.com/in/mbeuak/')
 }
 function repositorioGit(){
-    window.open('https://github.com/MBeuaK/OnePieceWeather')
+    open('https://github.com/MBeuaK/OnePieceWeather')
 }
 function DescobrirLocalizacao(){
-    alert("Funcionou")
+    let url;
+    navigator.geolocation.getCurrentPosition((pos) => {
+      let lat = pos.coords.latitude;
+      let long = pos.coords.longitude;
+      url = `https://api.hgbrasil.com/weather?format=json-cors&key=2c973d52&lat=-23.682&lon=-46.875&user_ip=remote`;
+      fetchApi(url);
+    });
+
+    function fetchApi(url) {
+        fetch(url)
+        .then((data) => {
+          return data.json();
+        })
+        .then((data) => {
+            temp = data.results.temp
+            hora = data.results.time;
+            msg.innerText = `${hora} ${temp} Cº`
+            carregar();
+        })
+
+  }
+
 }
-
-fetch("https://api.hgbrasil.com/weather")
-.then(function (response){
-    return response.json();
-})
-
-console.log(response.json)
